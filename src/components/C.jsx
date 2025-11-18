242b9341b6f8f9293ed8f0d695ac35eeca7489a8
wagheom509 <wagheom509@gmail.com>
2025-11-18 10:44:10 +0530

    updated project

--- src/components/C.jsx
+++ src/components/C.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function C() {
+   const[recipes,setRecipes]=useState(null)
+   useEffect(()=>{
+    axios.get("https://dummyjson.com/recipes")
+    .then(data=>setRecipes(data.data.recipes))
+
+
+
+   },[])
   return (
     <div>
-      
+
+        {recipes && recipes.map((r,i)=>{
+
+            return <>
+
+            <h1>{r.id} {r.name} hello</h1>
+            <img src={r.image} alt="" />
+            <hr />
+
+
+            
+            
+            
+            </>
+            
+        })}
     </div>
   )
 }
 
-export default C
+export default C
